fb745acf6b9311a3d6552b2c7d7d01f3a89116c1
Lester Gonzalez <retseltonyBB@bitbucket.org>
2020-10-21 16:19:08 +0200

    test

--- api-gateway/scripts/execCmdScript.sh
+++ api-gateway/scripts/execCmdScript.sh
@@ -4,6 +4,7 @@ PGSQL_DATA="/var/lib/pgsql/data/"
 echo "--->>> ${DB_PASSWORD}" 
 #set up master
     psql -Uwebadmin postgres -c "CREATE USER kong CREATEDB LOGIN ENCRYPTED PASSWORD '${DB_PASSWORD}';";
+    psql -c "\du"
     sudo /etc/init.d/postgresql stop
     sed -i "1i host kong  all        0.0.0.0/0    md5" ${PGSQL_DATA}pg_hba.conf;
     sudo /etc/init.d/postgresql start
