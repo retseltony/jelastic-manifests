3c42fd368563217cc5f6e302e2361e4c4cd0e172
Lester Gonzalez <retseltonyBB@bitbucket.org>
2020-10-22 23:12:38 +0200

    test again

--- api-gateway/scripts/registerKongaService.sh
+++ api-gateway/scripts/registerKongaService.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 KONGA_URL=$2
 KONG_URL=$1
-echo "KONG_URL=$KONG_URL <<-->> KONGA_URL=$KONGA_URL"
+
 curl --location --request POST "http://$KONG_URL:8001/services" --header 'Content-Type: application/json' --data-raw "{\"url\":\"http://$KONGA_URL\",\"name\":\"KONGA-UI-KONG-MANAGER\"}"
 curl --location --request POST "http://$KONG_URL:8001/routes" --header 'Content-Type: application/json' --data-raw '{"name":"Konga","protocols":["http","https"],"methods":["GET","POST","PUT","DELETE","OPTIONS","PATCH"],"paths":["/","/register"],"strip_path":false,"service":{"name":"KONGA-UI-KONG-MANAGER"}}'
