2c1304d1e37054871e0d63ef1eaf95097658d8da
Lester Gonzalez <retseltonyBB@bitbucket.org>
2020-10-22 14:29:02 +0200

    change the curl command for the services

--- api-gateway/scripts/registerKongaService.sh
+++ api-gateway/scripts/registerKongaService.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-KONGA_URL=$1
-curl --location --request POST 'https://localhost8444:/services' --header 'Content-Type: application/json' \
---data-raw '{"url":"http://$KONGA_URL","name":"KONGA-UI-KONG-MANAGER"}'
-curl --location --request POST 'https://localhost8444:/routes' --header 'Content-Type: application/json' \
---data-raw '{"name":"Konga","protocols":["http","https"],"methods":["GET","POST","PUT","DELETE","OPTIONS","PATCH"],"paths":["/","/register"],"strip_path":false,"service":{"name":"KONGA-UI-KONG-MANAGER"}}'
+KONGA_URL=$2
+KONG_URL=$1
+curl --location --request POST "http://$KONG_URL:8001/services" --header 'Content-Type: application/json' --data-raw "{\"url\":\"http://$KONGA_URL\",\"name\":\"KONGA-UI-KONG-MANAGER\"}"
+curl --location --request POST "https://$KONG_URL:8001/routes" --header 'Content-Type: application/json' --data-raw '{"name":"Konga","protocols":["http","https"],"methods":["GET","POST","PUT","DELETE","OPTIONS","PATCH"],"paths":["/","/register"],"strip_path":false,"service":{"name":"KONGA-UI-KONG-MANAGER"}}'
