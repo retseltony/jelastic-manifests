061341c8df30aea276f6f58ab2122ca71c9598cb
Lester Gonzalez <retseltonyBB@bitbucket.org>
2020-10-22 08:06:39 +0200

    creating database kongdb

--- api-gateway/scripts/execCmdScript.sh
+++ api-gateway/scripts/execCmdScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DB_PASSWORD=$1
 PGSQL_DATA="/var/lib/pgsql/data/"
-echo "--->>> ${DB_PASSWORD}" 
 #set up master
-    psql -Uwebadmin postgres -c "CREATE USER kong CREATEDB LOGIN ENCRYPTED PASSWORD '${DB_PASSWORD}';CREATE DATABASE kong_db WITH OWNER = kong ENCODING = 'UTF8';";
+    psql -Uwebadmin postgres -c "CREATE USER kong CREATEDB LOGIN ENCRYPTED PASSWORD '${DB_PASSWORD}';"
+    psql -Uwebadmin postgres -c "CREATE DATABASE kong_db WITH OWNER = kong ENCODING = 'UTF8';";
     sudo /etc/init.d/postgresql stop
     sed -i "1i host kong  all        0.0.0.0/0    md5" ${PGSQL_DATA}pg_hba.conf;
     sudo /etc/init.d/postgresql start
